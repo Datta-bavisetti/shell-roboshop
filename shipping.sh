8e4368c428c6e1fa7549acb505a0fb4f7bda73f9
DATTA BAVISETTI <dattabavisetti6304@gmail.com>
2026-02-19 12:05:21 +0530

    New Changes

--- shipping.sh
+++ shipping.sh
@@ -44,6 +44,9 @@ VALIDATE $? "Creating the app directory"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOGS_FILE
 VALIDATE $? "Downloading the app content"
 
+unzip /tmp/shipping.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip shipping code"
+
 cd /app &>>$LOGS_FILE
 VALIDATE $? "Chaging the directory to the app"
 
